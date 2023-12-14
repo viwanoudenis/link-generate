73c9428cad8c5509dc6c941fe45b2082b0611d91
denviwanou <89011384+denviwanou@users.noreply.github.com>
2023-12-14 18:05:24 +0100

    feat test

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -6,13 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class TestController extends AbstractController
 {
-    #[Route('/', name: 'home')]
+    /**
+     * @Route("/test", name="app_test")
+     */
     public function index(): Response
     {
-        return $this->render('home//index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'TestController',
         ]);
     }
 }
